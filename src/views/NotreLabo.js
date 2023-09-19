59b6025d900b4ec66e692386d5bed0fe4cd41e7c
Guillaume <gdaumur@next-lab.fr>
2023-09-19 16:01:48 +0200

    verif Clement

--- src/views/NotreLabo.js
+++ src/views/NotreLabo.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import '../utils/css/notreLabo.css';
+
+// Imports de toutes les images
+import imgBG from '../assets/images/notre-laboratoire/laboratoire.jpg'
+import machine1 from '../assets/images/notre-laboratoire/machine1.jpg'
 
 function NotreLabo() {
   return (
     <div>
-            <h1>Welcome to My NotreLabo</h1>
-            <p>This is a basic homepage for your application.</p>
+      <div className='img-bg-container'>
+        <img src={imgBG} className='imgBG'/>
+        <div className='text-container'>
+          <h1 className='titleBG'>
+            <span className='titleBG-bold'>LE FAÇONNIER</span> <br />
+            DE VOS COMPLÉMENTS <br />
+            ALIMENTAIRES
+          </h1>
+        </div>
+      </div>
+      <div className='container-1'>
+          <div className='container-1-img'>
+            {/* <img src={machine1} /> */}
+          </div>
+          <div className='container-1-text'>
+
+          </div>
+      </div>
     </div>
   );
 }
