ba61ed3ce0d0c67bb6308de2e3b8a192102d2517
Guillaume <gdaumur@next-lab.fr>
2023-09-19 09:01:01 +0200

    Header v1

--- src/views/FormuleSurMesure.js
+++ src/views/FormuleSurMesure.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function Homepage() {
+function FormuleSurMesure() {
   return (
     <div>
-            <h1>Welcome to My Homepage</h1>
+            <h1>Welcome to My FormuleSurMesure</h1>
             <p>This is a basic homepage for your application.</p>
     </div>
   );
 }
 
-export default Homepage;
+export default FormuleSurMesure;
--- src/views/Qualite.js
+++ src/views/Qualite.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function Homepage() {
+function Qualite() {
   return (
     <div>
-            <h1>Welcome to My Homepage</h1>
+            <h1>Welcome to My Qualite</h1>
             <p>This is a basic homepage for your application.</p>
     </div>
   );
 }
 
-export default Homepage;
+export default Qualite;
