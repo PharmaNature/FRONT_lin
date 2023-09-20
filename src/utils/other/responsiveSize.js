06ded000e337c6baa959ee7df590b6ce6bd85d17
Guillaume <gdaumur@next-lab.fr>
2023-09-20 14:26:01 +0200

    Header + Gestion Device Size

---
+++ src/utils/other/responsiveSize.js
@@ -0,0 +1,35 @@
+import data from "../json/imagesURL.js"
+
+function ResponsiveSize() {
+
+    const chooseImage = (page, windowWidth) => {
+
+        switch (true) {
+            case windowWidth < 420:
+                return data[page]["_phoneSize"];
+            case windowWidth >= 420 && windowWidth < 850:
+                return data[page]["_verticalTabSize"];
+            case windowWidth >= 850 && windowWidth < 1180:
+                return data[page]["_horizontalTabSize"];
+            default:
+                return data[page]["_desktopSize"];
+        }
+    };
+
+    const chooseSizeTitle = (windowWidth) => {
+
+        switch (true) {
+            case windowWidth < 420:
+                return "telSize";
+            case windowWidth >= 420 && windowWidth < 850:
+                return "verticalTabSize";
+            case windowWidth >= 850 && windowWidth < 1180:
+                return "horizontalTabSize";
+            default:
+                return "desktopSize";
+        }
+    };
+
+    return { chooseImage, chooseSizeTitle };
+}
+export default ResponsiveSize;
--- src/views/NotreLabo.js
+++ src/views/NotreLabo.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../utils/css/notreLabo.css';
-
-// Imports de toutes les images
-import imgBG from '../assets/images/notre-laboratoire/laboratoire.jpg'
-import machine1 from '../assets/images/notre-laboratoire/machine1.jpg'
+import ResponsiveSize from '../utils/other/responsiveSize'
 
 function NotreLabo() {
+  const { chooseImage, chooseSizeTitle } = ResponsiveSize();
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+      const handleResize = () => {
+          setWindowWidth(window.innerWidth);
+      };
+      window.addEventListener('resize', handleResize);
+
+      return () => {
+          window.removeEventListener('resize', handleResize);
+      };
+  }, []);
+
+  const imageChoice = chooseImage("labo", windowWidth);
+  const titleSizeChoice = chooseSizeTitle(windowWidth);
+
   return (
-    <div className='big-container'>
-      <div className='centered-image'>
-        <img src={imgBG} className='imgBG' alt="Laboratoire" />
-          <h1 className='centered-title'>
-            <span className='title-bold'>LE FAÇONNIER</span> <br />
-            DE VOS COMPLÉMENTS <br />
-            ALIMENTAIRES
-          </h1>
+      <div className='big-container'>
+          <div className='centered-image'>
+              <img src={imageChoice} className='imgBG' alt="Laboratoire" />
+              <h1 className={'centered-title ' + titleSizeChoice}>
+                  <span className='title-bold'>
+                      LE FAÇONNIER</span> <br />
+                  DE VOS COMPLÉMENTS <br />
+                  ALIMENTAIRES
+              </h1>
+          </div>
       </div>
-    </div>
   );
 }
 
